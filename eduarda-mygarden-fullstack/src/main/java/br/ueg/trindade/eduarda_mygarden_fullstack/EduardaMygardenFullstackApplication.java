8c71d7737e568f9d01c0f5cb79823bad55672b79
eduardarosa07 <erosa8614@gmail.com>
2026-08-21 21:40:26 -0300

    esqueleto do projeto

---
+++ eduarda-mygarden-fullstack/src/main/java/br/ueg/trindade/eduarda_mygarden_fullstack/EduardaMygardenFullstackApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.eduarda_mygarden_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduardaMygardenFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduardaMygardenFullstackApplication.class, args);
+	}
+
+}
