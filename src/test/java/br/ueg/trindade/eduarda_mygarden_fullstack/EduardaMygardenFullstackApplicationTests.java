d020e5baebf0cf59ffc268cfd006a45e446bbd19
eduardarosa07 <erosa8614@gmail.com>
2026-08-21 23:53:11 -0300

    Estrutura inicial do código-fonte (esqueleto)

---
+++ src/test/java/br/ueg/trindade/eduarda_mygarden_fullstack/EduardaMygardenFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.eduarda_mygarden_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduardaMygardenFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
